21fdd11f06b509dc0177e4a5083218b9fef0971a
Kaisar Ahmmed <kaisar123k@gmail.com>
2023-03-16 11:45:26 +0600

    add database

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     use HasFactory;
+
+    public function featured()
+    {
+        $this->belongsTo(Media::class, 'featured_meida_id');
+    }
+
+    public function location()
+    {
+        $this->belongsTo(Location::class, 'location_id');
+    }
+
+    public function gallery()
+    {
+        $this->hasMany(Media::class, 'property_id');
+    }
 }
--- database/migrations/2023_03_13_081913_create_properties_table.php
+++ database/migrations/2023_03_13_081913_create_properties_table.php
@@ -14,12 +14,25 @@ return new class extends Migration
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('featured_image');
+            $table->unsignedBigInteger('featured_meida_id');
+            $table->unsignedBigInteger('location_id');
+
             $table->unsignedBigInteger('price');
             $table->unsignedBigInteger('sale')->default(1)->comment('0=rent,1=sale');
             $table->unsignedBigInteger('type')->default(1)->comment('0=land,1=apartment,2=villa');
-            $table->string('bedrooms')->nullable();
+            $table->unsignedBigInteger('bedrooms')->nullable();
+            $table->unsignedBigInteger('bathrooms')->nullable();
+            $table->unsignedBigInteger('net_sqm')->nullable();
+            $table->unsignedBigInteger('gross_sqm')->nullable();
+            $table->longText('overview');
+            $table->longText('why_buy')->nullable();
+            $table->unsignedBigInteger('pool')->nullable()->comment('0=no,1=private,2=public,3=both');
             $table->timestamps();
+
+            $table->longText('description');
+
+            $table->foreign('featured_meida_id')->references('id')->on('media');
+            $table->foreign('location_id')->references('id')->on('locations');
         });
     }
 
